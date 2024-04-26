0da94add2ca0b78bf6c8d37d2b08b72aa553b73e
jul003 <dzulkiflifaiz11@gmail.com>
2024-04-26 15:56:45 +0700

    valomoal

--- js/controller/get.js
+++ js/controller/get.js
@@ -1,6 +1,6 @@
 import { addInner } from "https://bukulapak.github.io/element/process.js";
 import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
-import { isiTabel } from "js/temp/table.js";
+import { isiTabel } from "../temp/table.js";
 export function isiTablePresensi(results) {
     results.forEach(isiRow);
 }
