9332d48ebdef18e7f14bab1f29ecff2cd2433703
jul003 <dzulkiflifaiz11@gmail.com>
2024-06-11 05:37:51 +0700

    subuhhh

---
+++ js/config/url_get_detail.js
@@ -0,0 +1,5 @@
+
+const urlParams = new URLSearchParams(window.location.search);
+const presensiId = urlParams.get("presensiId");
+
+export let urlFetch = "https://ws-jul2024-bdc7cc35640a.herokuapp.com/presensi" + presensiId;
